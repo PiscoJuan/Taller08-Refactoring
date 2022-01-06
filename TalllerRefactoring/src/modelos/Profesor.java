baefac24cb2ffeeb1c6d6315d3f1b169c9559a37
Lurapozo <lurapozo@espol.edu.ec>
2022-01-06 10:47:57 -0500

    Se realizaron algunas tecnicas de refactoring

--- TalllerRefactoring/src/modelos/Profesor.java
+++ TalllerRefactoring/src/modelos/Profesor.java
@@ -9,9 +9,11 @@ public class Profesor {
     public int edad;
     public String direccion;
     public String telefono;
-    public InformacionAdicionalProfesor info;
     public ArrayList<Paralelo> paralelos;
-
+    private int aniosdeTrabajo;
+    private String facultad;
+    private double BonoFijo;
+	static double bonoPorAnio=600;
     public Profesor(String codigo, String nombre, String apellido, String facultad, int edad, String direccion, String telefono) {
         this.codigo = codigo;
         this.nombre = nombre;
@@ -25,6 +27,26 @@ public class Profesor {
     public void anadirParalelos(Paralelo p){
         paralelos.add(p);
     }
-    
-    
+    public int getAniosdeTrabajo() {
+    	return aniosdeTrabajo;
+    }
+    public String getFacultad() {
+    	return facultad;
+    }
+    public double getBonoFijo() {
+    	return BonoFijo;
+    } 
+    public void setAniosdeTrabajo(int anios) {
+    	this.aniosdeTrabajo=anios;
+    }
+    public void setFacultad(String facultad) {
+    	this.facultad=facultad;
+    }
+    public void setBonoFijo(double bonoFijo) {
+    	this.BonoFijo=bonoFijo;
+    }    
+
+    public double calcularSueldo(Profesor prof){
+        return prof.getAniosdeTrabajo()*bonoPorAnio + prof.getBonoFijo();
+    }      
 }
